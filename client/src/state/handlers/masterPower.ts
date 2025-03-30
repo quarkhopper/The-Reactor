31d0b2c3fbf7f694a4656e250a5ed5bb27f22faf
Dan <quarkhopper@gmail.com>
2025-03-30 16:54:22 -0700

    guard added to ignore master button presses in wrong phase

--- client/src/state/handlers/masterPower.ts
+++ client/src/state/handlers/masterPower.ts
@@ -5,38 +5,29 @@ import testRegistry from '../testRegistry';
 export function handleMasterPower() {
   const state = stateMachine.getAppState();
 
+  if (state === 'init' || state === 'startup' || state === 'shutdown') {
+    console.log(`[masterPower] Ignored press during '${state}'`);
+    return;
+  }
+
   if (state === 'off') {
     stateMachine.setAppState('init');
     stateMachine.log('System initializing');
 
-    // Begin init phase
     initRegistry.begin(() => {
       stateMachine.setAppState('startup');
       stateMachine.log('Init complete. Beginning startup test sequence.');
 
-      // Begin test registry phase
       testRegistry.begin(() => {
         stateMachine.setAppState('on');
         stateMachine.log('Startup test complete. System is now ON.');
       });
-
-      // Dispatch AFTER testRegistry is ready and all components have mounted
-      const testEvent = new CustomEvent('ui-event', {
-        detail: { type: 'test' },
-      });
-      window.dispatchEvent(testEvent);
     });
 
-    // Dispatch init event
     const initEvent = new CustomEvent('ui-event', {
       detail: { type: 'init' },
     });
     window.dispatchEvent(initEvent);
-  }
-
-  else if (state === 'startup') {
-    stateMachine.setAppState('on');
-    stateMachine.log('Startup complete. System is now on.');
   }
 
   else if (state === 'on') {
