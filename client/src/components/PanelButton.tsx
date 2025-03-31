f8696b82d5cabaa81be0e5f3941efbd5556ea4ed
Dan <quarkhopper@gmail.com>
2025-03-30 21:31:38 -0700

    checkpoint

--- client/src/components/PanelButton.tsx
+++ client/src/components/PanelButton.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import eventBus from '../state/eventBus';
 import coreSystem from '../state/subsystems/coreSystem';
+import initRegistry from '../state/initRegistry';
+import testRegistry from '../state/testRegistry';
 import glow_off from '../images/button_off.png';
 import glow_green from '../images/button_glow_green.png';
 import glow_amber from '../images/button_glow_amber.png';
@@ -29,21 +31,35 @@ const PanelButton: React.FC<PanelButtonProps> = ({ id, x, y, label }) => {
   const [disabled, setDisabled] = useState(false);
 
   useEffect(() => {
-    const match = id.match(/^fuel_rod_button_(\d+)_(\d+)$/);
-    if (!match) return;
-
-    const fx = parseInt(match[1]);
-    const fy = parseInt(match[2]);
-
-    const updateState = () => {
-      const ui = coreSystem.getState().getFuelRodUIState(fx, fy);
-      setDisplayColor(ui.color);
-      setDisabled(ui.locked);
+    const handleUiEvent = (e: Event) => {
+      const detail = (e as CustomEvent).detail;
+      if (detail.type === 'init') initRegistry.acknowledge(id);
+      if (detail.type === 'test') testRegistry.acknowledge(id);
     };
 
-    updateState();
-    const interval = setInterval(updateState, 200);
-    return () => clearInterval(interval);
+    window.addEventListener('ui-event', handleUiEvent);
+
+    const match = id.match(/^fuel_rod_button_(\d+)_(\d+)$/);
+    if (match) {
+      const fx = parseInt(match[1]);
+      const fy = parseInt(match[2]);
+
+      const updateState = () => {
+        console.log(`[button ${id}] ui state:`, coreSystem.getState().getFuelRodUIState(fx, fy));
+        const ui = coreSystem.getState().getFuelRodUIState(fx, fy);
+        setDisplayColor(ui.color);
+        setDisabled(ui.locked);
+      };
+
+      updateState();
+      const interval = setInterval(updateState, 200);
+      return () => {
+        clearInterval(interval);
+        window.removeEventListener('ui-event', handleUiEvent);
+      };
+    }
+
+    return () => window.removeEventListener('ui-event', handleUiEvent);
   }, [id]);
 
   const handleMouseDown = () => {
